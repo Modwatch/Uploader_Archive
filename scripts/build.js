3a7bd443a55ce29f19eb7c1b9a626e52f726e56b
Aaron Ballard <ans.ballard@gmail.com>
2016-05-01 15:29:13 -0500

    transpile scripts, add missing deps

--- scripts/build.js
+++ scripts/build.js
@@ -1,46 +1,49 @@
-const rollup = require("rollup");
-const nodeResolve = require("rollup-plugin-node-resolve");
-const babel = require("rollup-plugin-babel");
-const commonjs = require("rollup-plugin-commonjs");
-const rollupJSON = require("rollup-plugin-json");
-const uglify = require("uglify-js");
-const chalk = require("chalk");
-const program = require("commander");
-const watch = require("watch");
-const ora = require("ora");
-const keypress = require("keypress");
-const fs = require("fs");
-const denodeify = require("denodeify");
+"use strict";
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+var rollup = require("rollup");
+var nodeResolve = require("rollup-plugin-node-resolve");
+var babel = require("rollup-plugin-babel");
+var commonjs = require("rollup-plugin-commonjs");
+var rollupJSON = require("rollup-plugin-json");
+var uglify = require("uglify-js");
+var chalk = require("chalk");
+var program = require("commander");
+var watch = require("watch");
+var ora = require("ora");
+var keypress = require("keypress");
+var fs = require("fs");
+var denodeify = require("denodeify");
 
 fs.writeFile = denodeify(fs.writeFile);
 
-program
-.option("-w, --watch [Directory]", "Watch for changes")
-.option("-m, --minify", "Minify")
-.parse(process.argv);
+program.option("-w, --watch [Directory]", "Watch for changes").option("-m, --minify", "Minify").parse(process.argv);
+
+var spinner = void 0;
 
 keypress(process.stdin);
-if(program.watch) {
-  const spinner = ora("Initial Build");
+if (program.watch) {
+  spinner = ora("Initial Build");
   spinner.start();
 }
 
 build();
 
-if(program.watch) {
-  watch.watchTree(program.watch, (f, curr, prev) => {
-    if (typeof f == "object" && prev === null && curr === null) {
+if (program.watch) {
+  watch.watchTree(program.watch, function (f, curr, prev) {
+    if ((typeof f === "undefined" ? "undefined" : _typeof(f)) === "object" && prev === null && curr === null) {
       // finished walking tree
     } else {
-      spinner.text = "File changed, rebuilding...";
-      build()
-    }
+        spinner.text = "File changed, rebuilding...";
+        build();
+      }
   });
 }
 
-if(program.watch) {
-  process.stdin.on("keypress", (ch, key) => {
-    if (key &&  key.name === "q") {
+if (program.watch) {
+  process.stdin.on("keypress", function (ch, key) {
+    if (key && key.name === "q") {
       spinner.stop();
       watch.unwatchTree(program.watch);
       console.log(chalk.cyan("Cleanly stopped watch"));
@@ -55,29 +58,24 @@ if(program.watch) {
 function build() {
   return rollup.rollup({
     entry: "src/cli.js",
-    onwarn: program.watch ? e => { spinner.text = e; } : undefined,
-    plugins: [
-      nodeResolve({
-        preferBuiltins: true
-      }),
-      rollupJSON({
-        include: "node_modules/iconv-lite/**"
-      }),
-      commonjs(),
-      babel({
-        exclude: "node_modules/**",
-        babelrc: false,
-        presets: ["es2015-rollup"]
-      })
-    ]
-  })
-  .then(bundle => bundle.generate({
-    sourceMap: true,
-    banner: "#! /usr/bin/env node",
-    format: "cjs"
-  }))
-  .then(obj => {
-    if(program.minify) {
+    onwarn: program.watch ? function (e) {
+      spinner.text = e;
+    } : undefined,
+    plugins: [nodeResolve({
+      preferBuiltins: true
+    }), rollupJSON(), commonjs(), babel({
+      exclude: ["node_modules/**", "*.json"],
+      babelrc: false,
+      presets: ["es2015-rollup"]
+    })]
+  }).then(function (bundle) {
+    return bundle.generate({
+      sourceMap: true,
+      banner: "#! /usr/bin/env node",
+      format: "cjs"
+    });
+  }).then(function (obj) {
+    if (program.minify) {
       return uglify.minify(obj.code, {
         fromString: true,
         screwIe8: true,
@@ -87,22 +85,17 @@ function build() {
     } else {
       return obj;
     }
-  })
-  .then(obj => Promise.all([
-    obj.code.length,
-    fs.writeFile("./dist/cli.js", obj.code),
-    fs.writeFile("./dist/cli.js.map", obj.map)
-  ]))
-  .then(stats => {
-    if(!program.watch) {
-      console.log(chalk.blue(`File size: ${stats[0]} bytes`));
+  }).then(function (obj) {
+    return Promise.all([obj.code.length, fs.writeFile("./dist/cli.js", obj.code), fs.writeFile("./dist/cli.js.map", obj.map)]);
+  }).then(function (stats) {
+    if (!program.watch) {
+      console.log(chalk.blue("File size: " + stats[0] + " bytes"));
     } else {
-      spinner.text = `Built: File Size ${stats[0]} bytes`;
+      spinner.text = "Built: File Size " + stats[0] + " bytes";
     }
     return stats;
-  })
-  .catch(e => {
-    if(program.watch) {
+  }).catch(function (e) {
+    if (program.watch) {
       spinner.text = chalk.red(e);
     } else {
       console.log(chalk.red(e));
---
+++ src/scripts/build.js
@@ -0,0 +1,116 @@
+const rollup = require("rollup");
+const nodeResolve = require("rollup-plugin-node-resolve");
+const babel = require("rollup-plugin-babel");
+const commonjs = require("rollup-plugin-commonjs");
+const rollupJSON = require("rollup-plugin-json");
+const uglify = require("uglify-js");
+const chalk = require("chalk");
+const program = require("commander");
+const watch = require("watch");
+const ora = require("ora");
+const keypress = require("keypress");
+const fs = require("fs");
+const denodeify = require("denodeify");
+
+fs.writeFile = denodeify(fs.writeFile);
+
+program
+.option("-w, --watch [Directory]", "Watch for changes")
+.option("-m, --minify", "Minify")
+.parse(process.argv);
+
+let spinner;
+
+keypress(process.stdin);
+if(program.watch) {
+  spinner = ora("Initial Build");
+  spinner.start();
+}
+
+build();
+
+if(program.watch) {
+  watch.watchTree(program.watch, (f, curr, prev) => {
+    if (typeof f === "object" && prev === null && curr === null) {
+      // finished walking tree
+    } else {
+      spinner.text = "File changed, rebuilding...";
+      build()
+    }
+  });
+}
+
+if(program.watch) {
+  process.stdin.on("keypress", (ch, key) => {
+    if (key &&  key.name === "q") {
+      spinner.stop();
+      watch.unwatchTree(program.watch);
+      console.log(chalk.cyan("Cleanly stopped watch"));
+      process.exit(0);
+    }
+  });
+
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+}
+
+function build() {
+  return rollup.rollup({
+    entry: "src/cli.js",
+    onwarn: program.watch ? e => {
+      spinner.text = e;
+    } : undefined,
+    plugins: [
+      nodeResolve({
+        preferBuiltins: true
+      }),
+      rollupJSON(),
+      commonjs(),
+      babel({
+        exclude: [
+          "node_modules/**",
+          "*.json"
+        ],
+        babelrc: false,
+        presets: ["es2015-rollup"]
+      })
+    ]
+  })
+  .then(bundle => bundle.generate({
+    sourceMap: true,
+    banner: "#! /usr/bin/env node",
+    format: "cjs"
+  }))
+  .then(obj => {
+    if(program.minify) {
+      return uglify.minify(obj.code, {
+        fromString: true,
+        screwIe8: true,
+        outSourceMap: "cli.js.map",
+        inSourceMap: obj.map
+      });
+    } else {
+      return obj;
+    }
+  })
+  .then(obj => Promise.all([
+    obj.code.length,
+    fs.writeFile("./dist/cli.js", obj.code),
+    fs.writeFile("./dist/cli.js.map", obj.map)
+  ]))
+  .then(stats => {
+    if(!program.watch) {
+      console.log(chalk.blue(`File size: ${stats[0]} bytes`));
+    } else {
+      spinner.text = `Built: File Size ${stats[0]} bytes`;
+    }
+    return stats;
+  })
+  .catch(e => {
+    if(program.watch) {
+      spinner.text = chalk.red(e);
+    } else {
+      console.log(chalk.red(e));
+    }
+  });
+}
