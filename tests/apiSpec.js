4f77b9ec1f7d6030934d7ce41b7ea01d06a40dcf
Aaron Ballard <ans.ballard@gmail.com>
2016-05-02 04:49:15 +0000

    covered api specs

--- tests/apiSpec.js
+++ tests/apiSpec.js
@@ -1,9 +1,38 @@
 import test from "ava";
 import express from "express";
-import { getUsers, getUserFiles, getUserFile, getUserProfile } from "../src/lib/api";
+import { getUsers, getUserFiles, getUserFile, getUserProfile, uploadMods } from "../src/lib/api";
 import { promiseLog } from "../src/lib/utils";
 
 const NUMBEROFUSERS = 20;
+const PROFILE = {
+	timestamp: Date.now(),
+	tag: "whatevr",
+	game: "skyrim",
+	enb: "Godrays",
+	score: 12
+};
+const FILES = [
+	"plugins",
+	"ini",
+	"prefsini"
+];
+const PLUGINS = [
+	"First Line.esp",
+	"Second Line.esp",
+	"Third Line.esm"
+];
+const INI = [
+	"[Category]",
+	"First Line=12",
+	"Second Line=13",
+	"Third Line=something"
+];
+const PREFSINI = [
+	"[Category]",
+	"First Line=12",
+	"Second Line=13",
+	"Third Line=something"
+];
 
 test.before(() => {
 	let app = express();
@@ -17,6 +46,25 @@ test.before(() => {
 			});
 		}
 		res.json(users);
+	});
+	app.get("/api/user/:username/profile", (req, res) => {
+		res.json(PROFILE);
+	});
+	app.get("/api/user/:username/files", (req, res) => {
+		res.json(FILES);
+	});
+	app.get("/api/user/:username/file/plugins", (req, res) => {
+		res.json(PLUGINS);
+	});
+	app.get("/api/user/:username/file/ini", (req, res) => {
+		res.json(INI);
+	});
+	app.get("/api/user/:username/file/prefsini", (req, res) => {
+		res.json(PREFSINI);
+	});
+	app.post("/loadorder", (req, res) => {
+		res.status(200);
+		res.end();
 	});
 	app.listen(3000, () => {
 		console.log("Test Server Started");
@@ -36,4 +84,55 @@ test("Get Users", async t => {
 	})
 	.then(users => users.length)
 	t.is(await p2, NUMBEROFUSERS);
+});
+
+test("Get User Profile", async t => {
+	const p = getUserProfile({
+		api: "http://localhost:3000",
+		username: "Peanut"
+	})
+	t.deepEqual(await p, PROFILE);
+});
+
+test("Get User Files", async t => {
+	const p = getUserFiles({
+		api: "http://localhost:3000",
+		username: "Peanut"
+	})
+	t.deepEqual(await p, FILES);
+});
+
+test("Get User File Contents", async t => {
+	const p1 = getUserFile({
+		api: "http://localhost:3000",
+		username: "Peanut",
+		filetype: "plugins"
+	})
+	t.deepEqual(await p1, PLUGINS);
+	const p2 = getUserFile({
+		api: "http://localhost:3000",
+		username: "Peanut",
+		filetype: "ini"
+	})
+	t.deepEqual(await p2, INI);
+	const p3 = getUserFile({
+		api: "http://localhost:3000",
+		username: "Peanut",
+		filetype: "prefsini"
+	})
+	t.deepEqual(await p3, PREFSINI);
+});
+
+test("Upload", async t => {
+	const p = uploadMods({
+		api: "http://localhost:3000",
+		username: "Peanut",
+		password: "password",
+		game: "skyrim",
+		plugins: PLUGINS,
+		ini: INI,
+		prefsini: PREFSINI
+	})
+	.then(res => res.status)
+	t.is(await p, 200);
 });
