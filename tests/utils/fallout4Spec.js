d1b124be755afe44dbf7b62572049e4a2be25ae1
Aaron Ballard <ans.ballard@gmail.com>
2016-05-08 10:59:25 -0500

    moved modfiles so the parsed version can be stored

--- tests/utils/fallout4Spec.js
+++ tests/utils/fallout4Spec.js
@@ -3,44 +3,44 @@ import { cleanModFile, analyzeFile, scanModDirectory } from "../../src/lib/utils
 
 test("analyzeFile returns correct fallout4 filenames", async t => {
 	const plugins = cleanModFile({
-		filepath: "../modfiles/fallout4/plugins.txt"
+		filepath: "../modfiles/fallout4/in/plugins.txt"
 	}).then(m => m.filename);
 	t.is(await plugins, "plugins.txt");
 
 	const modlist = cleanModFile({
-		filepath: "../modfiles/fallout4/modlist.txt"
+		filepath: "../modfiles/fallout4/in/modlist.txt"
 	}).then(m => m.filename);
 	t.is(await modlist, "modlist.txt");
 
 	const fallout4 = cleanModFile({
-		filepath: "../modfiles/fallout4/fallout4.ini"
+		filepath: "../modfiles/fallout4/in/fallout4.ini"
 	}).then(m => m.filename);
 	t.is(await fallout4, "fallout4.ini");
 
 	const fallout4Prefs = cleanModFile({
-		filepath: "../modfiles/fallout4/fallout4prefs.ini"
+		filepath: "../modfiles/fallout4/in/fallout4prefs.ini"
 	}).then(m => m.filename);
 	t.is(await fallout4Prefs, "fallout4prefs.ini");
 });
 
 test("analyzeFile returns correct fallout4 file lengths", async t => {
 	const plugins = cleanModFile({
-		filepath: "../modfiles/fallout4/plugins.txt"
+		filepath: "../modfiles/fallout4/in/plugins.txt"
 	}).then(m => m.content.length);
 	t.is(await plugins, 4);
 
 	const modlist = cleanModFile({
-		filepath: "../modfiles/fallout4/modlist.txt"
+		filepath: "../modfiles/fallout4/in/modlist.txt"
 	}).then(m => m.content.length);
 	t.is(await modlist, 4);
 
 	const fallout4 = cleanModFile({
-		filepath: "../modfiles/fallout4/fallout4.ini"
+		filepath: "../modfiles/fallout4/in/fallout4.ini"
 	}).then(m => m.content.length);
 	t.is(await fallout4, 6);
 
 	const fallout4Prefs = cleanModFile({
-		filepath: "../modfiles/fallout4/fallout4prefs.ini"
+		filepath: "../modfiles/fallout4/in/fallout4prefs.ini"
 	}).then(m => m.content.length);
 	t.is(await fallout4Prefs, 9);
 });
--- tests/utils/skyrimSpec.js
+++ tests/utils/skyrimSpec.js
@@ -3,44 +3,44 @@ import { cleanModFile, scanModDirectory } from "../../src/lib/utils";
 
 test("analyzeFile returns correct skyrim filenames", async t => {
 	const plugins = cleanModFile({
-		filepath: "../modfiles/skyrim/plugins.txt"
+		filepath: "../modfiles/skyrim/in/plugins.txt"
 	}).then(m => m.filename);
 	t.is(await plugins, "plugins.txt");
 
 	const modlist = cleanModFile({
-		filepath: "../modfiles/skyrim/modlist.txt"
+		filepath: "../modfiles/skyrim/in/modlist.txt"
 	}).then(m => m.filename);
 	t.is(await modlist, "modlist.txt");
 
 	const skyrim = cleanModFile({
-		filepath: "../modfiles/skyrim/skyrim.ini"
+		filepath: "../modfiles/skyrim/in/skyrim.ini"
 	}).then(m => m.filename);
 	t.is(await skyrim, "skyrim.ini");
 
 	const skyrimPrefs = cleanModFile({
-		filepath: "../modfiles/skyrim/skyrimprefs.ini"
+		filepath: "../modfiles/skyrim/in/skyrimprefs.ini"
 	}).then(m => m.filename);
 	t.is(await skyrimPrefs, "skyrimprefs.ini");
 });
 
 test("analyzeFile returns correct skyrim file lengths", async t => {
 	const plugins = cleanModFile({
-		filepath: "../modfiles/skyrim/plugins.txt"
+		filepath: "../modfiles/skyrim/in/plugins.txt"
 	}).then(m => m.content.length);
 	t.is(await plugins, 5);
 
 	const modlist = cleanModFile({
-		filepath: "../modfiles/skyrim/modlist.txt"
+		filepath: "../modfiles/skyrim/in/modlist.txt"
 	}).then(m => m.content.length);
 	t.is(await modlist, 4);
 
 	const skyrim = cleanModFile({
-		filepath: "../modfiles/skyrim/skyrim.ini"
+		filepath: "../modfiles/skyrim/in/skyrim.ini"
 	}).then(m => m.content.length);
 	t.is(await skyrim, 6);
 
 	const skyrimPrefs = cleanModFile({
-		filepath: "../modfiles/skyrim/skyrimprefs.ini"
+		filepath: "../modfiles/skyrim/in/skyrimprefs.ini"
 	}).then(m => m.content.length);
 	t.is(await skyrimPrefs, 9);
 });
